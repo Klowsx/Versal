02209d2f4b1c9cd25ae5a75fc838344050fa7d43
JoelCastilloo <135539496+JoelCastilloo@users.noreply.github.com>
2025-07-17 00:40:45 -0500

    Update app.js
    inclusion de creacion de historia en el app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,18 @@
-const fastify = require("fastify")();
 const dotenv = require("dotenv");
+const fastify = require("fastify")();
 const jwt = require("@fastify/jwt");
 const connectDB = require("./src/config/db");
 const userRoutes = require("./src/modules/users/user.routes");
 const authPlugin = require("./src/plugins/auth.plugin");
+const storyCreationRoutes = require("./src/modules/createStory/storyCreation.routes");
 dotenv.config();
 connectDB();
 fastify.register(jwt, { secret: process.env.JWT_SECRET });
 
 fastify.register(authPlugin);
 fastify.register(userRoutes, { prefix: "/api/auth" });
+fastify.register(storyCreationRoutes, { prefix: "/api/story" });
+
 
 fastify.listen({ port: 3000 }, (err, address) => {
   if (err) {
